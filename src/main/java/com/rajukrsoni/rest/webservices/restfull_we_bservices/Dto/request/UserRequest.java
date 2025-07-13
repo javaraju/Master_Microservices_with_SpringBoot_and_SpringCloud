e64f8f5d641c0796a9f846eb3dcd9dd0837fdaca
rajukrsoni <info.krrajusoni@gmail.com>
2025-07-13 22:48:57 +0530

    Creating User Entity and some test data using H2 database,configure H2 database

--- src/main/java/com/rajukrsoni/rest/webservices/restfull_we_bservices/Dto/request/UserRequest.java
+++ src/main/java/com/rajukrsoni/rest/webservices/restfull_we_bservices/Dto/request/UserRequest.java
@@ -1,17 +1,27 @@
 package com.rajukrsoni.rest.webservices.restfull_we_bservices.Dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class UserRequest {
+
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2, message = "Name should have atleast 2 characters")
     @JsonProperty("user_name")
+    @Column(name = "user_name")
     private String name;
     @Past(message = "Birth Date should be in the past")
+    @Column(name = "birth_date")
     private LocalDate birthdate;
 
     public UserRequest(Integer id, String name, LocalDate birthdate) {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into user_details(id,birth_date,user_name) values (101,current_date(),'Raju kumar');
+
+insert into user_details(id,birth_date,user_name) values (102,current_date(),'Ranu');
+
+insert into user_details(id,birth_date,user_name) values (103,current_date(),'Pihu');
+
+insert into user_details(id,birth_date,user_name) values (104,current_date(),'Kaju');
